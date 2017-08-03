410d4b08490077bc07486c0171cf2f84cc6a26b7
GongXincheng <757853223@qq.com>
2017-08-03 14:20:49 +0800

    唯一约束
    unique

--- mysql_1.sql
+++ mysql_1.sql
@@ -2,28 +2,37 @@ CREATE DATABASE day05;
 USE day05;
 
 -- 主键约束
-
--- 方式1：
+	-- 方式1：
 	CREATE TABLE student(
 		id INT PRIMARY KEY,
 		NAME VARCHAR(50)
 	);
 	
--- 方式2：适合创建联合主键
-CREATE TABLE student(
-	classid INT,
-	id INT,
-	NAME VARCHAR(50),
-	PRIMARY KEY(id, classid)
-);
+	-- 方式2：适合创建联合主键
+	CREATE TABLE student(
+		classid INT,
+		id INT,
+		NAME VARCHAR(50),
+		PRIMARY KEY(id, classid)
+	);
 
--- 方式3：
-CREATE TABLE student(
-	id INT,
-	NAME VARCHAR(50)
-);
-ALTER TABLE student ADD CONSTRAINT pk_stu_id PRIMARY KEY(id);
+	-- 方式3：
+	CREATE TABLE student(
+		id INT,
+		NAME VARCHAR(50)
+	);
+	ALTER TABLE student ADD CONSTRAINT pk_stu_id PRIMARY KEY(id);
+
+
+-- 唯一约束：
+
+	CREATE TABLE student(
+		id INT PRIMARY KEY,
+		NAME VARCHAR(50) UNIQUE
+	);
+	
 
 
 -- 删除数据库表
 DROP TABLE student;
+SELECT * FROM student;
