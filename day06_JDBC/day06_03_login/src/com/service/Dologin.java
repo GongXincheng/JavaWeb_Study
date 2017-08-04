f82c45600470432fb011a2ea84e37c85270eeaae
GongXincheng <757853223@qq.com>
2017-08-04 15:58:06 +0800

    PreparedStatement

--- day06_JDBC/day06_03_login/src/com/service/Dologin.java
+++ day06_JDBC/day06_03_login/src/com/service/Dologin.java
@@ -1,9 +1,9 @@
 package com.service;
 
 import java.sql.Connection;
+import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
-import java.sql.Statement;
 import com.entity.User;
 import com.util.DBUtils;
 
@@ -16,14 +16,20 @@ public class Dologin {
 	 */
 	public User findUser(String name, String pwd){
 		Connection conn = null;
-		Statement stmt = null;
+		PreparedStatement pstmt = null;
 		ResultSet rs = null;
 		User user = null;
 		
 		try {
 			conn = DBUtils.getConnection();	//得到连接对象
-			stmt = conn.createStatement();	//得到执行sql语句的对象
-			rs = stmt.executeQuery("select * from users where name='"+name+"' and password='"+pwd+"' ");//执行sql语句
+			String sql = "select * from users where name=? and password=? ";
+			pstmt = conn.prepareStatement(sql);	//得到执行sql语句的对象
+			
+			//给？赋值
+			pstmt.setString(1, name);
+			pstmt.setString(2, pwd);
+			
+			rs = pstmt.executeQuery();//执行sql语句
 			if(rs.next()){
 				user = new User();
 				user.setId(rs.getInt("id"));
@@ -36,7 +42,7 @@ public class Dologin {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}finally{
-			DBUtils.closeAll(rs,stmt,conn);
+			DBUtils.closeAll(rs,pstmt,conn);
 		}
 		return user;
 	}
