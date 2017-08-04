bdc2745a7114508da71c1de81d088eed28a3b992
GongXincheng <757853223@qq.com>
2017-08-04 11:26:07 +0800

    获取Connection的三种方式
    String url = "jdbc:mysql://localhost:3306/dayo6";
    
    (ur;l,"root","123")
    
    Properties info = new Properties ();
    info.setProperty("user", "root");
    info.setProperty("password", "123");
    (url ,info)

--- day06_JDBC/day06_00_demo1/src/com/jdbcdemo/Demo2.java
+++ day06_JDBC/day06_00_demo1/src/com/jdbcdemo/Demo2.java
@@ -4,6 +4,9 @@ import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.ResultSet;
 import java.sql.Statement;
+import java.util.Properties;
+
+import org.junit.Test;
 
 /*
  * 2017年8月4日 10:14:29
@@ -12,7 +15,8 @@ import java.sql.Statement;
  */
 public class Demo2 {
 
-	public static void main(String[] args) throws Exception {
+	@Test
+	public void test1() throws Exception{
 
 		//加载驱动
 		Class.forName("com.mysql.jdbc.Driver");
@@ -41,5 +45,71 @@ public class Demo2 {
 		stmt.close();
 		conn.close();
 	}
+	
+	@Test
+	public void test2() throws Exception{
 
+		//加载驱动
+		Class.forName("com.mysql.jdbc.Driver");
+		
+		//获取连接 Connection
+//		Connection conn = DriverManager.getConnection("jdbc:mysql://localhost:3306/day06", "root", "123");
+		Properties info = new Properties();
+		info.setProperty("user", "root");
+		info.setProperty("password", "123");
+		Connection conn = DriverManager.getConnection("jdbc:mysql://localhost:3306/day06", info);
+		
+		//得到执行sql语句的对象 statement
+		Statement stmt = conn.createStatement();
+		
+		//执行sql语句，并返回结果
+		ResultSet rs = stmt.executeQuery("select * from users");
+		
+		//处理结果
+		while(rs.next()){
+			System.out.print(rs.getObject(1)+"\t");
+			System.out.print(rs.getObject(2)+"\t");
+			System.out.print(rs.getObject(3)+"\t");
+			System.out.print(rs.getObject(4)+"\t");
+			System.out.print(rs.getObject(5)+"\t");
+			System.out.println();
+		}
+		
+		//关闭资源
+		rs.close();
+		stmt.close();
+		conn.close();
+	}
+	
+	
+	@Test
+	public void test3() throws Exception{
+
+		//加载驱动
+		Class.forName("com.mysql.jdbc.Driver");
+		
+		//获取连接 Connection
+		Connection conn = DriverManager.getConnection("jdbc:mysql://localhost:3306/day06?user=root&password=123");
+		
+		//得到执行sql语句的对象 statement
+		Statement stmt = conn.createStatement();
+		
+		//执行sql语句，并返回结果
+		ResultSet rs = stmt.executeQuery("select * from users");
+		
+		//处理结果
+		while(rs.next()){
+			System.out.print(rs.getObject(1)+"\t");
+			System.out.print(rs.getObject(2)+"\t");
+			System.out.print(rs.getObject(3)+"\t");
+			System.out.print(rs.getObject(4)+"\t");
+			System.out.print(rs.getObject(5)+"\t");
+			System.out.println();
+		}
+		
+		//关闭资源
+		rs.close();
+		stmt.close();
+		conn.close();
+	}
 }
