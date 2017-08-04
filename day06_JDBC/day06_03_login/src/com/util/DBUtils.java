00cd545f0c377692d72d5b651216805372241e24
GongXincheng <757853223@qq.com>
2017-08-04 14:41:25 +0800

    DBUtils

---
+++ day06_JDBC/day06_03_login/src/com/util/DBUtils.java
@@ -0,0 +1,64 @@
+package com.util;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.ResourceBundle;
+
+public class DBUtils {
+	private static String driverClass;
+	private static String url;
+	private static String user;
+	private static String password;
+	
+	static{
+		ResourceBundle rb = ResourceBundle.getBundle("dbinfo");
+		//给四个变量赋值
+		driverClass = rb.getString("driverClass");
+		url = rb.getString("url");
+		user = rb.getString("user");
+		password = rb.getString("password");
+		try {
+			Class.forName(driverClass);
+		} catch (ClassNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+	
+	public static Connection getConnection() throws SQLException{
+		return DriverManager.getConnection(url, user, password);
+	}
+	
+	public void closeAll(ResultSet rs, Statement stmt, Connection conn){
+		if(rs!=null){
+			try {
+				rs.close();
+			} catch (SQLException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+			rs = null;
+		}
+		if(stmt!=null){
+			try {
+				stmt.close();
+			} catch (SQLException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+			stmt = null;
+		}
+		if(conn!=null){
+			try {
+				conn.close();
+			} catch (SQLException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+			conn = null;
+		}
+	}
+}
