a5710fabc836cc90e98f72ea48a2b2257754d5ea
GongXincheng <757853223@qq.com>
2017-08-05 22:38:00 +0800

    设置编码方式，及生命周期

--- day08_servlet/day08_00_servlet/src/com/servlet/ServletDemo1.java
+++ day08_servlet/day08_00_servlet/src/com/servlet/ServletDemo1.java
@@ -1,6 +1,7 @@
 package com.servlet;
 
 import java.io.IOException;
+import java.io.PrintWriter;
 
 import javax.servlet.Servlet;
 import javax.servlet.ServletConfig;
@@ -10,39 +11,50 @@ import javax.servlet.ServletResponse;
 
 public class ServletDemo1 implements Servlet{
 
-	@Override
-	public void service(ServletRequest arg0, ServletResponse arg1)
-			throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		
-		System.out.println("Hello servlet !");
-		
+	//Servlet生命周期的方法，实例化
+	//在Servlet 第一次 被访问时调用
+	public ServletDemo1(){
+		System.out.println("****** ServletDemo1 ***** 实例化 ****");
 	}
 	
+	//Servlet生命周期的方法，初始化
+	//在Servlet 第一次 被访问时调用
+	@Override
+	public void init(ServletConfig arg0) throws ServletException {
+		// TODO Auto-generated method stub
+		System.out.println("****** init ***** 初始 ****");
+	}
 	
+	//Servlet生命周期的方法，服务方法
+	//每次访问时都会被调用
+	@Override
+	public void service(ServletRequest request, ServletResponse response)
+			throws ServletException, IOException {
+		request.setCharacterEncoding("utf-8");
+		response.setContentType("text/html;charset=utf-8");
+		
+		PrintWriter out = response.getWriter();
+		System.out.println("service 服务执行 !");
+		out.print("service 服务执行 !");
+	}
+	
+	//Servlet生命周期的方法，销毁
 	@Override
 	public void destroy() {
-		// TODO Auto-generated method stub
-		
+		System.out.println("****** destroy ***** 销毁 ****");
 	}
 
+	
 	@Override
 	public ServletConfig getServletConfig() {
-		// TODO Auto-generated method stub
 		return null;
 	}
 
 	@Override
 	public String getServletInfo() {
-		// TODO Auto-generated method stub
 		return null;
 	}
 
-	@Override
-	public void init(ServletConfig arg0) throws ServletException {
-		// TODO Auto-generated method stub
-		
-	}
 
 	
 }
