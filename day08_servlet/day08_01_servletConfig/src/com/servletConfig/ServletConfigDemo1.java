6f04f7c339f9554d8c1a746697afd7ec316a679f
GongXincheng <757853223@qq.com>
2017-08-06 19:20:56 +0800

    获取配置文件中的信息
    init(config)
    
    config.getInitParameter("");

---
+++ day08_servlet/day08_01_servletConfig/src/com/servletConfig/ServletConfigDemo1.java
@@ -0,0 +1,32 @@
+package com.servletConfig;
+
+import java.io.IOException;
+
+import javax.servlet.ServletConfig;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class ServletConfigDemo1 extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+	private ServletConfig config;
+
+	@Override
+	public void init(ServletConfig config) throws ServletException {
+		// TODO Auto-generated method stub
+		this.config = config;
+	}
+
+	public void doGet(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		String encoding = config.getInitParameter("encoding");	//获取配置文件的信息
+		System.out.println("配置文件的值："+encoding);
+	}
+
+	public void doPost(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+
+	}
+
+}
