dfe5702758e5a1d59d49495fc0a204df37de7576
GongXincheng <757853223@qq.com>
2017-08-06 19:32:58 +0800

    三种获取config的方式
    1. servlet中的 init函数 获取ServletConfig对象
    
    2. HttpServlet 对象的this.getServletConfig方法获取 ServletConfig对象
    
    3. 直接在本类 this.getInitParameter("属性名");

--- day08_servlet/day08_01_servletConfig/src/com/servletConfig/ServletConfigDemo1.java
+++ day08_servlet/day08_01_servletConfig/src/com/servletConfig/ServletConfigDemo1.java
@@ -10,17 +10,25 @@ import javax.servlet.http.HttpServletResponse;
 
 public class ServletConfigDemo1 extends HttpServlet {
 	private static final long serialVersionUID = 1L;
-	private ServletConfig config;
+//	private ServletConfig config;
 
 	@Override
-	public void init(ServletConfig config) throws ServletException {
-		// TODO Auto-generated method stub
-		this.config = config;
-	}
+//	public void init(ServletConfig config) throws ServletException {
+//		// TODO Auto-generated method stub
+//		this.config = config;
+//	}
 
 	public void doGet(HttpServletRequest request, HttpServletResponse response)
 			throws ServletException, IOException {
-		String encoding = config.getInitParameter("encoding");	//获取配置文件的信息
+//		String encoding = config.getInitParameter("encoding");	//获取配置文件的信息
+//		System.out.println("配置文件的值："+encoding);
+		
+		//第二种
+		String encoding1 = this.getServletConfig().getInitParameter("encoding");
+		System.out.println("配置文件的值："+encoding1); 
+		
+		//第三种方式
+		String encoding = this.getInitParameter("encoding");
 		System.out.println("配置文件的值："+encoding);
 	}
 
