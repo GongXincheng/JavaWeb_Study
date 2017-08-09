6687ab3a8f8d085e3b2f6fed3701716f0e7a29aa
GongXincheng <757853223@qq.com>
2017-08-09 16:29:20 +0800

    获取session中的数据

---
+++ day10_cookie&session/day10_01_HttpSession/src/com/servlet/SessionDemo2.java
@@ -0,0 +1,36 @@
+package com.servlet;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+public class SessionDemo2 extends HttpServlet {
+
+	public void doGet(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		
+		//设置编码方式并获取对象
+		response.setContentType("text/html; charset=UTF-8");
+		PrintWriter out = response.getWriter();
+		HttpSession session = request.getSession();
+		
+		String name = (String)session.getAttribute("name");
+		if(name!=null){
+			out.println(name);
+		}else{
+			out.println("你不能直接访问此功能");
+		}
+	}
+
+	public void doPost(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+
+		doGet(request, response);
+	}
+
+}
