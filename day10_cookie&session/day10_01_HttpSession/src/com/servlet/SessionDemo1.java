829168c90c20119150a6af82881c3ac15a030be3
GongXincheng <757853223@qq.com>
2017-08-09 16:29:03 +0800

    将数据封装到session中

--- day10_cookie&session/day10_01_HttpSession/src/com/servlet/SessionDemo1.java
+++ day10_cookie&session/day10_01_HttpSession/src/com/servlet/SessionDemo1.java
@@ -6,12 +6,19 @@ import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 
 public class SessionDemo1 extends HttpServlet {
 
 	public void doGet(HttpServletRequest request, HttpServletResponse response)
 			throws ServletException, IOException {
-
+		String name = request.getParameter("name");
+		//解决url乱码
+		name = new String(name.getBytes("ISO-8859-1"),"UTF-8");
+		//得到一个HttpSession对象
+		HttpSession session = request.getSession();
+		session.setAttribute("name", name);
+		
 	}
 
 	public void doPost(HttpServletRequest request, HttpServletResponse response)
