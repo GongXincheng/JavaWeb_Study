aac6b035e409467fd49065209db4bca6d8e3689e
GongXincheng <757853223@qq.com>
2017-08-11 21:42:19 +0800

    DAO层接口类型
    Data Access Object

---
+++ day12_JavaWeb设计模式/day12_user/src/com/dao/UserDao.java
@@ -0,0 +1,21 @@
+package com.dao;
+
+import com.domain.User;
+
+public interface UserDao {
+	
+	/**
+	 * 添加用户信息
+	 * @param user
+	 * @throws Exception
+	 */
+	public void addUser(User user) throws Exception;
+	
+	/**
+	 * 根据用户名和密码查找用户信息
+	 * @param user
+	 * @return
+	 * @throws Exception
+	 */
+	public User findUser(User user) throws Exception;
+}
