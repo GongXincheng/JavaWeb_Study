d98050b8e3a2f8fa977ba427f19f85425b4243f8
GongXincheng <757853223@qq.com>
2017-08-14 15:48:21 +0800

    数据库连接处池实现javax.sql.DataSource接口,/得到一个包装后的MyConnection对象
    创建LinkedList<Connection>集合

---
+++ day13_事务与连接池/day13_00_pool/src/com/datasource/MyDataSource.java
@@ -0,0 +1,96 @@
+package com.datasource;
+
+import java.io.PrintWriter;
+import java.sql.Connection;
+import java.sql.SQLException;
+import java.sql.SQLFeatureNotSupportedException;
+import java.util.Collections;
+import java.util.LinkedList;
+import java.util.logging.Logger;
+
+import javax.sql.DataSource;
+
+import com.util.DBUtil;
+
+public class MyDataSource implements DataSource{
+
+	private static LinkedList<Connection> pool = (LinkedList<Connection>) Collections.synchronizedList(new LinkedList<Connection>());
+
+	static {
+		try {
+			for (int i = 0; i < 10; i++) {
+				Connection conn;
+				conn = DBUtil.getConnection();
+				pool.add(conn);
+			}
+		} catch (Exception e) {
+			throw new ExceptionInInitializerError("初始化数据路连接失败,请检查配置文件是否正确");
+		}
+	}
+	
+	@Override
+	public Connection getConnection() throws SQLException {
+		Connection conn = null;
+		if(pool.size()>0){
+			conn = pool.removeFirst();	//从池中取出一个连接
+			Connection myConn = new MyConnection(conn, pool);//得到一个包装后的MyConnection
+			return myConn;
+		}
+		else{
+			//等待
+			//新创建一个连接
+			throw new RuntimeException("服务器繁忙");
+		}
+	}
+
+
+	@Override
+	public Connection getConnection(String username, String password)
+			throws SQLException {
+		// TODO Auto-generated method stub
+		return null;
+	}
+	
+	@Override
+	public PrintWriter getLogWriter() throws SQLException {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void setLogWriter(PrintWriter out) throws SQLException {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void setLoginTimeout(int seconds) throws SQLException {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public int getLoginTimeout() throws SQLException {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public Logger getParentLogger() throws SQLFeatureNotSupportedException {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public <T> T unwrap(Class<T> iface) throws SQLException {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public boolean isWrapperFor(Class<?> iface) throws SQLException {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+}
