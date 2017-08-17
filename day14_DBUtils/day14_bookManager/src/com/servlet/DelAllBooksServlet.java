73996a64af64b5eb88429ab97ebdd426016684d2
GongXincheng <757853223@qq.com>
2017-08-17 18:46:05 +0800

    DelAllBooksServlet

--- day14_DBUtils/day14_bookManager/src/com/servlet/DelAllBooksServlet.java
+++ day14_DBUtils/day14_bookManager/src/com/servlet/DelAllBooksServlet.java
@@ -1,31 +1,29 @@
 package com.servlet;
 
 import java.io.IOException;
-import java.util.List;
 
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-import com.domain.Book;
 import com.service.BookServiceImpl;
 
-public class BookListServlet extends HttpServlet {
+public class DelAllBooksServlet extends HttpServlet {
 
 	public void doGet(HttpServletRequest request, HttpServletResponse response)
 			throws ServletException, IOException {
-		request.setCharacterEncoding("utf-8");
-		response.setContentType("text/html; charset=UTF-8");
+		//得到所有id
+		String[] ids = request.getParameterValues("ids");
 		
-		//调用业务逻辑
-		BookServiceImpl  bsi = new BookServiceImpl();
-		List<Book> list = bsi.findAllBooks();
-		//跳转页面
-		if(list!=null && list.size()>0){
-			request.setAttribute("books", list); //把list放入request对象中
-			request.getRequestDispatcher("/admin/products/list.jsp").forward(request, response);
-		}
+		//调用删除业务
+		BookServiceImpl bs = new BookServiceImpl();
+//		for (int i = 0; i < ids.length; i++) {
+//			bs.deleteBook(ids[i]);
+//		}
+		bs.deleteAllBooks(ids);
+		//分发转向
+		request.getRequestDispatcher("bookListServlet").forward(request, response);
 	}
 
 	public void doPost(HttpServletRequest request, HttpServletResponse response)
