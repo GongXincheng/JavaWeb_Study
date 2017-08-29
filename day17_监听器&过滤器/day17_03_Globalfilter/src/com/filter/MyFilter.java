4a8d44b9fd0b30dec1301b3c407b12ce666a117e
GongXincheng <757853223@qq.com>
2017-08-29 23:51:09 +0800

    装饰设计模式，完成全局编码过滤器
    饰设计模式
    1. 实现与被包装对象相同的接口
    2. 定义一个与被包装类相同对象的引用
    3. 定义一个构造方法，把被包装对象传过来
    4. 对于不需要该写的方法，直接调用
    5. 对于需要该写的方法,写自己的方法

--- day17_监听器&过滤器/day17_03_Globalfilter/src/com/filter/MyFilter.java
+++ day17_监听器&过滤器/day17_03_Globalfilter/src/com/filter/MyFilter.java
@@ -1,6 +1,7 @@
 package com.filter;
 
 import java.io.IOException;
+import java.io.UnsupportedEncodingException;
 
 import javax.servlet.Filter;
 import javax.servlet.FilterChain;
@@ -9,6 +10,7 @@ import javax.servlet.ServletException;
 import javax.servlet.ServletRequest;
 import javax.servlet.ServletResponse;
 import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletRequestWrapper;
 import javax.servlet.http.HttpServletResponse;
 
 public class MyFilter implements Filter{
@@ -24,14 +26,47 @@ public class MyFilter implements Filter{
 		HttpServletRequest req = (HttpServletRequest)request;
 		HttpServletResponse res = (HttpServletResponse)response;
 		
-		req.setCharacterEncoding("UTF-8");
-		res.setContentType("text/html; charset=UTF-8");
-		chain.doFilter(request, response);
+		//解决post编码方式
+//		req.setCharacterEncoding("UTF-8");
+//		res.setContentType("text/html; charset=UTF-8");
+		
+		req = new MyRequest(req);
+		
+		chain.doFilter(req, response);
 	}
 
 	@Override
 	public void destroy() {
 		
 	}
+}
 
+/*	装饰设计模式
+ * 
+ * 1. 实现与被包装对象相同的接口
+ * 2. 定义一个与被包装类相同对象的引用
+ * 3. 定义一个构造方法，把被包装对象传过来
+ * 4. 对于不需要该写的方法，直接调用
+ * 5. 对于需要该写的方法,写自己的方法
+ */
+class MyRequest extends HttpServletRequestWrapper{
+	HttpServletRequest request;
+	
+	public MyRequest(HttpServletRequest request) {
+		super(request);
+		this.request = request;
+	}
+
+	@Override
+	public String getParameter(String name) {
+		name = request.getParameter(name);
+		try {
+			return new String(name.getBytes("iso-8859-1"),"UTF-8");
+		} catch (UnsupportedEncodingException e) {
+			e.printStackTrace();
+		}
+		return null;
+	}
+
+	
 }
