271609c4b7b034846de12399ff9574dfa532f338
GongXincheng <757853223@qq.com>
2017-09-02 16:55:01 +0800

    package的属性，测试

---
+++ day24_struts2_1/day24_ee04_struts2_package/src/com/web/action/Demo1Action.java
@@ -0,0 +1,12 @@
+package com.web.action;
+
+public class Demo1Action {
+	public String saveUser(){
+		System.out.println("saveUser1");
+		return "success";
+	}
+	public String saveUser2(){
+		System.out.println("saveUser2");
+		return "success";
+	}
+}
