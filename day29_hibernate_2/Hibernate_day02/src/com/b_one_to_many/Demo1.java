306212337a2944b096e39726a54c9b4cda3eb129
GongXincheng <757853223@qq.com>
2017-10-02 23:55:56 +0800

    inverse property

--- day29_hibernate_2/Hibernate_day02/src/com/b_one_to_many/Demo1.java
+++ day29_hibernate_2/Hibernate_day02/src/com/b_one_to_many/Demo1.java
@@ -1,5 +1,7 @@
 package com.b_one_to_many;
 
+import java.util.Set;
+
 import org.hibernate.Session;
 import org.hibernate.Transaction;
 import org.junit.Test;
@@ -11,9 +13,8 @@ import com.utils.HibernateUtils;
 public class Demo1 {
 
 	@Test	//保存操作
-	//出现2条sql语句：
-	//3条：insert
-	//2条：update
+	//(inverse="false") 3条：insert，2条：update
+	//(inverse="true") 3条：insert ：外键由Order维护
 	public void fun1(){
 		
 		Session session = HibernateUtils.openSession();
@@ -23,13 +24,15 @@ public class Demo1 {
 		
 		Customer c = new Customer();
 		c.setName("tom");
+		
 		Order o1 = new Order();
 		o1.setName("肥皂");
 		Order o2 = new Order();
 		o2.setName("蜡烛");
 		
-		c.getOrders().add(o1);
-		c.getOrders().add(o2);
+/*		c.getOrders().add(o1);	//Customer维护外键关系
+		c.getOrders().add(o2); 						*/		
+		
 		o1.setCustomer(c);
 		o2.setCustomer(c);
 		
@@ -44,4 +47,31 @@ public class Demo1 {
 		
 	}
 	
+	@Test //删除
+	//结论：维护一方的对象时，会自动维护另一方的关系
+	//如果将Customer配置文件中的 inverse="true"时，会报错
+		//原因：Customer不再维护外键，直接删除Customer违反外键约束
+	public void fun2(){
+		
+		Session session = HibernateUtils.openSession();
+		Transaction transaction = session.beginTransaction();
+		
+		//----------------------------------------------------
+		
+		Customer c = (Customer)session.get(Customer.class, 4);
+		
+		//当 inverse="true" 时
+		Set<Order> set = c.getOrders();
+		for(Order o : set){
+			o.setCustomer(null);
+		}
+		
+		session.delete(c);
+		
+		//----------------------------------------------------
+		
+		transaction.commit();
+		session.close();
+		
+	}
 }
