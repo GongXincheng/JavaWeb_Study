e0a39dfae3da024636056e276b7f2e4a7b7a9643
GongXincheng <757853223@qq.com>
2017-10-04 20:35:29 +0800

    Hibernate transaction filter

---
+++ day29_hibernate_4/Hibernate_tieba/src/com/web/filter/TransactionFilter.java
@@ -0,0 +1,49 @@
+package com.web.filter;
+
+import java.io.IOException;
+
+import javax.servlet.Filter;
+import javax.servlet.FilterChain;
+import javax.servlet.FilterConfig;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+
+import org.hibernate.Session;
+import org.hibernate.Transaction;
+
+import com.utils.HibernateUtil;
+
+public class TransactionFilter implements Filter{
+
+	public void destroy() {
+		
+	}
+
+	public void doFilter(ServletRequest request, ServletResponse response,
+			FilterChain chain) throws IOException, ServletException {
+		
+		Session session = HibernateUtil.openSession();
+		Transaction transaction = session.beginTransaction();
+		
+		try {
+			
+			chain.doFilter(request, response);
+			
+			if(session!=null && session.isOpen()){
+				transaction.commit();
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
+			if(session!=null && session.isOpen()){
+				transaction.rollback();
+			}
+		}
+		
+	}
+
+	public void init(FilterConfig arg0) throws ServletException {
+		
+	}
+
+}
