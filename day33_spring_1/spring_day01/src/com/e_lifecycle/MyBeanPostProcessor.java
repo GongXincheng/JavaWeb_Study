0e5fa18469357fa200139390f6163c5fa4d6cc26
GongXincheng <757853223@qq.com>
2017-10-11 00:52:08 +0800

    后处理bean

---
+++ day33_spring_1/spring_day01/src/com/e_lifecycle/MyBeanPostProcessor.java
@@ -0,0 +1,45 @@
+package com.e_lifecycle;
+
+import java.lang.reflect.InvocationHandler;
+import java.lang.reflect.Method;
+import java.lang.reflect.Proxy;
+
+import org.springframework.beans.BeansException;
+import org.springframework.beans.factory.config.BeanPostProcessor;
+
+public class MyBeanPostProcessor implements BeanPostProcessor {
+	@Override
+	public Object postProcessBeforeInitialization(Object bean, String beanName)
+			throws BeansException {
+		System.out.println("前方法");
+		return bean;
+	}
+	
+	@Override
+	public Object postProcessAfterInitialization(final Object bean, String beanName)
+			throws BeansException {
+		System.out.println("后方法");
+		// bean 为目标对象
+		// 生成 jdk 代理对象
+		return Proxy.newProxyInstance(MyBeanPostProcessor.class.getClassLoader(),
+				bean.getClass().getInterfaces(), 
+				new InvocationHandler() {
+					@Override
+					public Object invoke(Object proxy, Method method, Object[] args)
+							throws Throwable {
+						
+						System.out.println("-----开启事务---------");
+						
+						Object object = method.invoke(bean, args);
+						
+						System.out.println("-----提交事务---------");
+						return object;
+					}
+				});
+	}
+
+
+	
+	
+
+}
--- day33_spring_1/spring_day01/src/com/e_lifecycle/TestCycle.java
+++ day33_spring_1/spring_day01/src/com/e_lifecycle/TestCycle.java
@@ -6,14 +6,17 @@ import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 public class TestCycle {
 	
-	@SuppressWarnings("resource")
+	
 	@Test
-	public void demo1(){
+	public void demo1() throws Exception{
 		
 		String xmlPath = "com/e_lifecycle/beans.xml";
 		ApplicationContext applicationContext = new ClassPathXmlApplicationContext(xmlPath);
 		UserService userService = applicationContext.getBean("userServiceId", UserService.class);
 		userService.addUser();
+		
+		//要求：1.容器必须close，才能执行销毁方法，2.必须是单例的(scope="singleton")
+		applicationContext.getClass().getMethod("close").invoke(applicationContext);
 	}
 	
 }
